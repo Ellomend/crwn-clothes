b1bb5f333535e9b4232beb761276999870c73e47
abogonosov <a.bogonosov@pangeoradar.ru>
2020-12-18 03:59:52 +0300

    #122 update redux add item to cart

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 import './CollectionItem.scss'
+import CustomButton from "../CustomButton/CustomButton"
+import { connect } from "react-redux"
+import { addItem } from "../../redux/cart/cartActions"
 
-const CollectionItem = ({id, name, price, imageUrl}) => {
+const CollectionItem = ({item, addItem}) => {
+  const {imageUrl, price, name} = item
   return (
     <div className="collection-item">
       <div className="image" style={{backgroundImage: `url(${imageUrl})`}}/>
@@ -10,8 +14,12 @@ const CollectionItem = ({id, name, price, imageUrl}) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = {
+  addItem: addItem
+}
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import React from "react"
+import { Link } from "react-router-dom"
+import { connect } from "react-redux"
 
-import { ReactComponent as Logo } from "../../assets/crown.svg";
-import './Header.scss'
-import { auth } from '../../firebase/firebase.utils'
-import CartIcon from '../CartIcon/CartIcon'
-import CartDropdown from '../CartDropdown/CartDropdown'
+import { ReactComponent as Logo } from "../../assets/crown.svg"
+import "./Header.scss"
+import { auth } from "../../firebase/firebase.utils"
+import CartIcon from "../CartIcon/CartIcon"
+import CartDropdown from "../CartDropdown/CartDropdown"
 
 const Header = ({ currentUser, hidden }) => {
   const handleSignOut = () => {
@@ -15,31 +15,34 @@ const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
-        <Logo className="logo"/>
+        <Logo className="logo" />
       </Link>
       <div className="options">
-        <Link className='option' to='/shop'>SHOP</Link>
-        <Link className='option' to='/shop'>CONTACT</Link>
-        {
-          currentUser ?
-            <div className='option' onClick={handleSignOut}>SIGN OUT</div>
-            :
-            <Link className='option' to='/signin'>SIGN IN</Link>
-        }
-        <CartIcon/>
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={handleSignOut}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
+        <CartIcon />
       </div>
-      {
-        hidden ? null :
-          <CartDropdown/>
-      }
-
+      {hidden ? null : <CartDropdown />}
     </div>
-  );
-};
+  )
+}
 
 const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
   currentUser,
   hidden
 })
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header)

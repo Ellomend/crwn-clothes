b42b583434bf3e6a0f301b3cade0afb107433dd9
abogonosov <a.bogonosov@pangeoradar.ru>
2020-12-18 22:41:56 +0300

    #140 move directory data to redux

--- src/pages/ShopPage/ShopPage.js
+++ src/pages/ShopPage/ShopPage.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react';
-import { SHOP_DATA } from './ShopPageData'
+import React from 'react';
 import CollectionPreview from '../../components/CollectionPreview/CollectionPreview'
+import { connect } from "react-redux"
 
-class ShopPage extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-  render () {
-    const { collections } = this.state
-    return (
-      <div className="shop-page">
-        {
-          collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview {...otherCollectionProps} key={id}/>
-          ))
-        }
-      </div>
-    );
-  }
+
+const ShopPage = ({directory}) => {
+  return (
+    <div className="shop-page">
+      {
+        directory.map(({id, ...otherCollectionProps}) => (
+          <CollectionPreview {...otherCollectionProps} key={id}/>
+        ))
+      }
+    </div>
+  )
 }
 
-export default ShopPage;
+const mapStateToProps = ({directory}) => ({
+  directory
+})
+
+export default connect(mapStateToProps)(ShopPage)
+
+
+// class ShopPage extends Component {
+//   constructor (props) {
+//     super(props);
+//     this.state = {
+//       collections: SHOP_DATA
+//     }
+//   }
+//   render () {
+//     const { collections } = this.state
+//     return (
+//       <div className="shop-page">
+//         {
+//           collections.map(({id, ...otherCollectionProps}) => (
+//             <CollectionPreview {...otherCollectionProps} key={id}/>
+//           ))
+//         }
+//       </div>
+//     );
+//   }
+// }
+
+// export default ShopPage;
